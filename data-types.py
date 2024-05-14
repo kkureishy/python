fddebd01b183de96493d573e643d9ba22ca7d9e2
kkureishy <kkureishy@students.mchenry.edu>
2024-05-14 12:02:28 -0500

    Add files via upload

---
+++ data-types.py
@@ -0,0 +1,22 @@
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 100
+print("Literal:", x)
+
+# Integer
+y = -9
+print("Integer:", y)
+
+# Float
+z = 3.14159
+print("Float:", z)
+
+# String
+name = "Quangdong Tigers"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
