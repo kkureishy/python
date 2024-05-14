f38f4feb26fe9ebf9a6f4c80dc57aee6cd0b0fd5
kkureishy <kkureishy@students.mchenry.edu>
2024-05-14 12:46:28 -0500

    Add files via upload

---
+++ mathematical-operations.py
@@ -0,0 +1,11 @@
+#length
+length = 30
+print("\tLength: ", length)
+#width
+width = 15
+print("\tWidth: ", width)
+#area
+area = length * width
+print("\tArea: ", area)
+#message with calculated area
+print("The area of the rectangle with a length of " + str(length) + " and a width of " + str(width) + " is " + str(area) +".")
