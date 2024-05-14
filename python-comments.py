41f5ce5986d651fc9e56ba442c75c1b5c24387b7
kkureishy <kkureishy@students.mchenry.edu>
2024-05-14 13:05:07 -0500

    Add files via upload

---
+++ python-comments.py
@@ -0,0 +1,4 @@
+tank_size = 15 #size of tank in gallons
+distance = 600 # distance traveled in miles
+mpg = distance / tank_size # miles per gallon fromula
+print("Miles per gallon:", mpg) # message outputing mpg
