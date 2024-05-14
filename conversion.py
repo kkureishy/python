8c5e432258e5f09ed47ea6c8230e38e641248070
kkureishy <kkureishy@students.mchenry.edu>
2024-05-14 12:57:55 -0500

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,17 @@
+#kilograms given
+kilograms1 = 100
+kilograms2 = 3
+kilograms3 = 54
+kilograms4 = 90
+#conversion
+conversion = 2.20462
+# pounds calculaiton
+pounds1 = kilograms1 * conversion
+pounds2 = kilograms2 * conversion
+pounds3 = kilograms3 * conversion
+pounds4 = kilograms4 * conversion
+# message 
+print("So, " + str(kilograms1) + " kilograms is approximately equal to " + str(pounds1) + " pounds.")
+print("So, " + str(kilograms2) + " kilograms is approximately equal to " + str(pounds2) + " pounds.")
+print("So, " + str(kilograms3) + " kilograms is approximately equal to " + str(pounds3) + " pounds.")
+print("So, " + str(kilograms4) + " kilograms is approximately equal to " + str(pounds4) + " pounds.")
