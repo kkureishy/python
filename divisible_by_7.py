54204ce9a9c0f2020e0d5fc06bfb3eb4c11d3233
kkureishy <kkureishy@students.mchenry.edu>
2024-05-28 12:06:16 -0500

    Add files via upload

--- divisible_by_7.py
+++ divisible_by_7.py
@@ -1,4 +1,4 @@
-for i in range(1,300):
-    if i % 7 ==0:
-        print(str(i))
+for i in range(1,301): #loop through all numbers 1 through 300
+    if i % 7 ==0: #check to see if number is divisible by 7 (see if remainder is 0)
+        print(str(i)) #if divisible by 7 print number
     
