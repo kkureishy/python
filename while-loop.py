d791407e379e20d37db0c8807ff5cc8dc8e1a5d4
kkureishy <kkureishy@students.mchenry.edu>
2024-05-28 12:02:04 -0500

    Add files via upload

--- while-loop.py
+++ while-loop.py
@@ -1,20 +1,25 @@
+#start with bottles at 100
 bottle = 100
+#loop until all bottles are gone or 0
 while bottle >0:
+    #when amount of bottles get to 2 make sure the phrasing is grammatically correct (when at 1 bottle say 1 bottle of beer on the wall)
     if bottle ==2:
         print(str(bottle) + " bottles of beer on the wall")
         print(str(bottle) + " bottles of beer\nTake one down, pass it around")
-        bottle+=-1
+        bottle+=-1 # subtract amount of bottles after taking one
         print(str(bottle) + " bottle of beer on the wall!")
         print("")
+    #when amount of bottles get to 1 make sure the phrasing is grammatically correct (when at 1 bottle say 1 bottle of beer on the wall)
     elif bottle == 1:
         print(str(bottle) + " bottle of beer on the wall")
         print(str(bottle) + " bottle of beer\ntake it down, pass it around")
-        bottle+=-1
+        bottle+=-1 # subtract amount of bottles after taking one
         print("No bottles of beer on the wall!")
         print("")
+    #all else print phrase normally
     else:
         print(str(bottle) + " bottles of beer on the wall")
         print(str(bottle) + " bottles of beer\nTake one down, pass it around")
-        bottle+=-1
+        bottle+=-1 # subtract amount of bottles after taking one
         print(str(bottle) + " bottles of beer on the wall!")
         print("")
