28d3e2c07516e644415b4e9a8f80db7f4abf944d
kkureishy <kkureishy@students.mchenry.edu>
2024-06-07 13:29:01 -0500

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,12 @@
+#main function
+def main():
+    #tuple called programming class with 6 classes
+    programming_classes = ('Intro to Python', 'Advanced Python', 'Database Essentials', 'Web Development Basics', 'Data Structures in Python', 'Web Design Fundamentals')
+    #loop through each class in the tuple
+    for i in programming_classes:
+        #print each class
+        print(i)
+    #print length of programming classes
+    print(len(programming_classes))
+#call main function
+main()
